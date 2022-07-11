c04a73c48b9ae87427ceba8ac3c42b9c6289740e
Michael Wender <michael@michaelwender.com>
2022-07-11 14:15:39 -0400

    First commit

---
+++ sproutvideo-embed.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     SproutVideo Embed
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Provides a shortcode for embedding videos hosted on SproutVideo.
+ * Author:          TheWebist
+ * Author URI:      https://mwender.com
+ * Text Domain:     sproutvideo-embed
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sproutvideo_Embed
+ */
+
+// Your code starts here.
