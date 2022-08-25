16c9721748b54384e01d08e8b62904d4f4f75b2d
Michael Wender <michael@michaelwender.com>
2022-08-25 08:09:38 -0400

    Updating version number in header

--- sproutvideo-embed.php
+++ sproutvideo-embed.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     sproutvideo-embed
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.1
  *
  * @package         Sproutvideo_Embed
  */
